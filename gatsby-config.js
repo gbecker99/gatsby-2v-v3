cfc067b6e6af25138227c39eb8ab78dc4ec184f9
gbecker99 <54383476+gbecker99@users.noreply.github.com>
2021-06-06 16:33:16 +0800

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: '${_dirname}/src/pages'
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: '${_dirname}/src/posts'
+      },
+    },
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          name: 'images',
+          path: '${_dirname}/src/images'
+        },
+      },
+    ],
+  }
